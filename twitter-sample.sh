ff204636bb11179bd2aeb7bd6d56fdaf9a16ed40
boydbelshof <B.belshof@Hotmail.com>
2017-03-13 12:31:32 +0100

    Update twitter-sample.sh

--- twitter-sample.sh
+++ twitter-sample.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# A program that opens the corpus files.
+# A program that opens the corpus files from 1 march 2017 12 o'clock. It read the numbers of tweet, unique tweets, the retweets and it prints the first 20 tweets
 echo "Number of tweets in the file:"
- zless /net/corpora/twitter2/Tweets/2017/03/20170301\:12.out.gz  | /net/corpora/twitter2/tools/tweet2tab text | wc -l
+zless /net/corpora/twitter2/Tweets/2017/03/20170301\:12.out.gz  | /net/corpora/twitter2/tools/tweet2tab text | wc -l
 echo "Unique tweets in the file:"
 zless /net/corpora/twitter2/Tweets/2017/03/20170301\:12.out.gz  | /net/corpora/twitter2/tools/tweet2tab text | sort -u |wc -l
 echo "Number of retweets:"
