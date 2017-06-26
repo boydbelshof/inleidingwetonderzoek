65c5ac95d53cbd6b881640397826111b8ae56578
boydbelshof <B.belshof@Hotmail.com>
2017-06-26 15:06:07 +0200

    Update runme.sh

--- runme.sh
+++ runme.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # A program that opens the corpus files from 1 march 2017 12 o'clock. It read the numbers of tweet, unique tweets, the retweets and it prints the first 20 tweets
 #Run this file for sample data
-zless /net/corpora/twitter2/Tweets/2017/03/201703*.out.gz | /net/corpora/twitter2/tools/tweet2tab user.location text | sort -u | python code.py
+zless /net/corpora/twitter2/Tweets/2017/03/201703*.out.gz | /net/corpora/twitter2/tools/tweet2tab user.name text | sort -u | python code.py
 
